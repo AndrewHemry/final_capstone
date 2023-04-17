4e5e75b2499ff4252a523fced2cf75d860f1d097
AndrewHemry <Andrewhemry@gmail.com>
2023-04-16 20:37:01 -0500

    Still testing redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,19 @@
-// import { createStore } from "redux";
-// import reducers from "./reducers";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+import { createStore } from "redux";
+import reducers from "./reducers";
+import storage from "redux-persist/lib/storage";
 // import state from "./state";
 
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
+
 // export default createStore(reducers, state)
-
-import { createStore } from 'redux';
-import { persistStore } from 'redux-persist';
-import persistedReducer from './reducers';
-
-const store = createStore(persistedReducer);
-const persistor = persistStore(store);
-
-export { store, persistor };
