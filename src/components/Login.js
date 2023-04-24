0eec41e3128864039cec4e87b9a6ba26cea01486
AndrewHemry <Andrewhemry@gmail.com>
2023-04-24 10:23:18 -0500

    Reverting change for Auth on Front end

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,52 +6,53 @@ import cookie from "cookie";
 const Login = (props) => {
     const navigate = useNavigate();
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    // const [username, setUsername] = useState("");
+    // const [password, setPassword] = useState("");
 
-    // const [state, setState] = useState({
-    //     username: "",
-    //     password: ""
-    // })
+    const [state, setState] = useState({
+        username: "",
+        password: ""
+    })
 
-    // const handleTextChange = (e) => {
-    //     const { name, value } = e.target;
-    //     setState((prevState) => {
-    //         return {
-    //             ...prevState,
-    //             // I have a comma here in my other code but why?
-    //             [name]: value
-    //         }
-    //     })
-    // }
-
-    const login = async (e) => {
-        e.preventDefault();
-        const response = await fetch(process.env.REACT_APP_login, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ username, password }),
-        });
-        if (response.ok) {
-            const token = await response.json();
-            console.log("The token is:", token)
-            localStorage.setItem("token", token)
-            document.cookie = cookie.serialize("loggedIn", true, { maxAge: 600});
-            navigate("/");
-            window.location.reload(false);
-        } else {
-            alert("The username or password is incorrect.")
-        }
+    const handleTextChange = (e) => {
+        const { name, value } = e.target;
+        setState((prevState) => {
+            return {
+                ...prevState,
+                // I have a comma here in my other code but why?
+                [name]: value
+            }
+        })
     }
 
-    // const login = (e) => {
+    // ACTION ITEM - When I use the API login for this
+    // const login = async (e) => {
     //     e.preventDefault();
-    //     document.cookie = cookie.serialize("loggedIn", true, { maxAge: 600});
-    //     navigate("/");
-    //     window.location.reload(false);
+    //     const response = await fetch(process.env.REACT_APP_login, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: JSON.stringify({ username, password }),
+    //     });
+    //     if (response.ok) {
+    //         const token = await response.json();
+    //         console.log("The token is:", token)
+    //         localStorage.setItem("token", token)
+    //         document.cookie = cookie.serialize("loggedIn", true, { maxAge: 600});
+    //         navigate("/");
+    //         window.location.reload(false);
+    //     } else {
+    //         alert("The username or password is incorrect.")
+    //     }
     // }
+
+    const login = (e) => {
+        e.preventDefault();
+        document.cookie = cookie.serialize("loggedIn", true, { maxAge: 600});
+        navigate("/");
+        window.location.reload(false);
+    }
 
     return (
         <div className="login-page">
@@ -59,10 +60,10 @@ const Login = (props) => {
                 <form className="login-form" onSubmit={login}>
                     <TextField 
                         required
-                        // onChange={handleTextChange}
-                        onChange={(e) => setUsername(e.target.value)}
-                        // value={state.username}
-                        value={username}
+                        onChange={handleTextChange}
+                        // onChange={(e) => setUsername(e.target.value)}
+                        value={state.username}
+                        // value={username}
                         name="username"
                         label="Username"
                         type="text"
@@ -70,10 +71,10 @@ const Login = (props) => {
                     />
                     <TextField 
                         required
-                        // onChange={handleTextChange}
-                        onChange={(e) => setPassword(e.target.value)}
-                        // value={state.password}
-                        value={password}
+                        onChange={handleTextChange}
+                        // onChange={(e) => setPassword(e.target.value)}
+                        value={state.password}
+                        // value={password}
                         name="password"
                         label="Password"
                         type="password"
